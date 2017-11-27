d4fdb3c21b37aa9249cb0c86c9c9a24d4f7c327e
Jens Schoeler <jens.schoeler.90@gmail.com>
2017-11-27 12:11:33 +0100

    Change gdv.h

--- gdv/gdv.h
+++ gdv/gdv.h
@@ -1,12 +1,17 @@
 #pragma once
 #include "Drohne.h"
+<<<<<<< HEAD
 #include "Snowflake.h"
+=======
+#include "GL/SOIL.h"
+>>>>>>> 95f1d3882cafdf20077f747f233c812e2791af19
 #define CAMERASPEED 0.05f
-#define SPEEDOBJECT 0.001f
+#define SPEEDOBJECT .005f
 #define ESCAPEKEY 27
 #define GAMESPEED 2
 #define SCROLLUP 3
 #define SCROLLDOWN 4
+
 enum direction {LEFT,RIGHT,UP,DOWN,FORWARD,BACKWARD};
 
 
@@ -24,3 +29,4 @@ void moveCameraKeyboard(float,direction);
 void Animate(int);
 void idle();
 void moveDrone(int);
+void drawTree(float, int);
